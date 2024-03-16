46b2da9d6bf20faa034feeb8344d641dc7ce0b78
IWasimSoudan <128977819+IWasimSoudan@users.noreply.github.com>
2024-03-16 12:16:07 +0200

    add 6

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 // Navbar.js
+
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faBars } from "@fortawesome/free-solid-svg-icons";
+import { Navbar, Container, Nav } from "react-bootstrap";
 /*
 1-- home
 2-- about 
@@ -9,83 +9,68 @@ import { faEnvelope, faBars } from "@fortawesome/free-solid-svg-icons";
 4--SocialMedia
 5-- logIN
 */
-export default function Navbar(page) {
+
+const _navbar = (page) => {
   const { page: pageName } = page;
   return (
-    <nav className="navbar navbar-expand-lg sticky-top pt-35 pb-3">
-      <div className="container mb-2">
-        <a className="navbar-brand" href="#">
+    <Navbar expand="lg" sticky="top" className="pt-35 pb-3">
+      <Container className="mb-2">
+        <Navbar.Brand href="#">
           <img src={require("../../assets/img/logo/logo2_footer.png")} alt="" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <i className="fa-solid">
-            {" "}
-            <FontAwesomeIcon
-              icon={faBars}
-              className=" fa-lg  rounded-circle p-2 "
-            />
-          </i>
-        </button>
-        <div className="collapse navbar-collapse" id="main">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a
-                className={`nav-link p-lg-3 ${pageName === 1 ? "active" : ""}`}
-                aria-current="page"
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="ms-auto mb-2 mb-lg-0 ">
+            <Nav.Item>
+              <Nav.Link
                 href="/"
+                className={`  nav-link-custom nav-link p-lg-3 ${pageName === 1 ? "active " : ""}`}
               >
                 Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                className={`nav-link p-lg-3 ${pageName === 4 ? "active" : ""}`}
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
                 href="/socialmedia"
+                className={`nav-link p-lg-3 ${pageName === 4 ? "active" : ""}`}
               >
                 Social Media
-              </a>
-            </li>
-            {/* <li className="nav-item">
-              <a className="nav-link p-lg-3" href="#">
-                Portfolio
-              </a>
-            </li> */}
-            <li className="nav-item">
-              <a
-                className={`nav-link p-lg-3 ${pageName === 2 ? "active" : ""}`}
+              </Nav.Link>
+            </Nav.Item>
+            {/* <Nav.Item>
+              <Nav.Link href="#" className="nav-link p-lg-3">Portfolio</Nav.Link>
+            </Nav.Item> */}
+            <Nav.Item>
+              <Nav.Link
                 href="/about"
+                className={`nav-link p-lg-3 ${pageName === 2 ? "active" : ""}`}
               >
                 About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                href="/contact"
                 className={`nav-link p-lg-3 ${pageName === 3 ? "active" : ""}`}
-                href="contact"
               >
                 Contact
-              </a>
-            </li>
-          </ul>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
           <div className="search ps-3 pe-3 d-none d-lg-block">
             <i className="fa-solid fa-magnifying-glass"></i>
           </div>
           <a
-            className={`btn rounded-pill main-btn pt-4 pb-4 ${pageName === 5 ? "active" : ""}`}
             href="/login"
+            className={`btn rounded-pill main-btn pt-4 pb-4 ${pageName === 5 ? "active" : ""}`}
           >
             Login
           </a>
-        </div>
-      </div>
-    </nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-}
+};
+
+export default _navbar;
--- src/page/MainPage/MainPage.js
+++ src/page/MainPage/MainPage.js
@@ -1,5 +1,6 @@
 import Aggregator from "../../Aggregator/Aggregator";
 import Navbar from "../../component/Navbar/Navbar";
+
 import Footer from "../../component/Footer/Footer";
 import SingleBlog from "../../component/SingleBlog/singleBlog";
 const MainPage = () => {
